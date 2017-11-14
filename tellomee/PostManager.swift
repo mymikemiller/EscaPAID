c7a96bf3f42899e29813762d2470d29295ab1dcd
Michael Miller <mikem@Michaels-MacBook-Pro.local>
2017-11-14 09:43:30 -0700

    Chats now are one-on-one

--- tellomee/PostManager.swift
+++ tellomee/PostManager.swift
@@ -18,29 +18,36 @@ class PostManager: NSObject {
     static func addPost(username:String, text:String, toId:String, fromId:String) {
         let p = Post(username: username, text: text, toId: toId)
         if (p.text != "") {
-            let uid = Auth.auth().currentUser?.uid
-            let post = ["uid":uid!,
+            let chatName = getChatName(fromId: fromId, toId: toId)
+            let post = ["uid":fromId,
                         "username":p.username,
                         "text":p.text,
                         "toId":p.toId]
-            databaseRef.child("posts").childByAutoId().setValue(post)
+            databaseRef.child("posts").child(chatName).childByAutoId().setValue(post)
         }
-    }    
+    }
     
     static func fillPosts(uid:String?, toId:String, completion: @escaping(_ result:String) -> Void) {
+        let chatName = getChatName(fromId: uid!, toId: toId)
         
-        databaseRef.child("posts").queryOrdered(byChild: "uid").queryEqual(toValue: FirebaseManager.currentUser?.uid).observe(.childAdded, with:{
+        databaseRef.child("posts").child(chatName).observe(.childAdded, with:{
             snapshot in
             print(snapshot)
             if let result = snapshot.value as? [String:AnyObject]{
-                let toIdCloud = result["toId"]! as! String
-                if(toIdCloud == toId){
-                    let p = Post(username: result["username"]! as! String, text: result["text"]! as! String, toId: result["toId"]! as! String)
-                    PostManager.posts.append(p)
-                }
+                let p = Post(username: result["username"]! as! String, text: result["text"]! as! String, toId: result["toId"]! as! String)
+                PostManager.posts.append(p)
             }
             completion("")
         })
+    }
+    
+    private static func getChatName(fromId:String, toId:String) -> String {
+        // The chat name is what groups chats between two people together.
+        // The name is the alphabetical string "fromId - toId" or "toId - fromId"
+        if (fromId < toId) {
+            return fromId + " - " + toId
+        }
+        return toId + " - " + fromId
     }
 }
 
