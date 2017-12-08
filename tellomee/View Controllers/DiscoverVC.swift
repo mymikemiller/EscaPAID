7dd7892dc1c836bacf7816a5e68ec26d1b3d72dd
mymikemiller <mikem.exe@gmail.com>
2017-12-08 15:23:19 -0700

    Search and filtering in the Discover tab

--- tellomee/View Controllers/DiscoverVC.swift
+++ tellomee/View Controllers/DiscoverVC.swift
@@ -12,6 +12,10 @@ class DiscoverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var filteredExperiences = [Experience]()
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
     // This is set to the selected experience when the user selects a row. This is used to prepare for the segue.
     var selectedExperience: Experience?
     
@@ -21,11 +25,18 @@ class DiscoverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         let nib = UINib(nibName: "ExperienceCell",bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "experienceCell")
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Setup the Scope Bar
+        searchController.searchBar.scopeButtonTitles = ["All", "Adventure", "Lifestyle", "Cullinary"]
+        searchController.searchBar.delegate = self
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Experiences"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
         
         ExperienceManager.fillExperiences {
             () in
@@ -48,58 +59,55 @@ class DiscoverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ExperienceManager.experiences.count
+        if (isFiltering()) {
+            return filteredExperiences.count
+        } else {
+            return ExperienceManager.experiences.count
+        }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "experienceCell", for: indexPath) as! ExperienceTableViewCell
 
-        let experience = ExperienceManager.experiences[indexPath.row]
+        let experience: Experience
+        if isFiltering() {
+            experience = filteredExperiences[indexPath.row]
+        } else {
+            experience = ExperienceManager.experiences[indexPath.row]
+        }
+        
         cell.experience = experience
 
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedExperience = ExperienceManager.experiences[indexPath.row]
         self.performSegue(withIdentifier: "showExperience", sender: self)
     }
- 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredExperiences = ExperienceManager.experiences.filter({( experience : Experience) -> Bool in
+            let doesCategoryMatch = (scope == "All") || (experience.category == scope)
+            
+            if searchBarIsEmpty() {
+                return doesCategoryMatch
+            } else {
+                return doesCategoryMatch && experience.title.lowercased().contains(searchText.lowercased())
+            }
+        })
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func isFiltering() -> Bool {
+        let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
+        return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
     // MARK: - Navigation
 
@@ -107,10 +115,43 @@ class DiscoverVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        if (segue.identifier == "showExperience") {
-            (segue.destination as! ExperienceVC).experience = selectedExperience
+//        if (segue.identifier == "showExperience") {
+//            (segue.destination as! ExperienceVC).experience = selectedExperience
+//        }
+        
+        if segue.identifier == "showExperience" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let experience: Experience
+                if isFiltering() {
+                    experience = filteredExperiences[indexPath.row]
+                } else {
+                    experience = ExperienceManager.experiences[indexPath.row]
+                }
+                let controller = segue.destination as! ExperienceVC
+                controller.experience = experience
+//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
         }
     }
     
 
 }
+
+
+extension DiscoverVC: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+    }
+}
+
+extension DiscoverVC: UISearchBarDelegate {
+    // MARK: - UISearchBar Delegate
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+    }
+}
+
