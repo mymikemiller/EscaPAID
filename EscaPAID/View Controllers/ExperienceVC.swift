4217d60f5658310271793dd3cf38aac3a61a1c43
Michael J. Miller <mikem.exe@gmail.com>
2018-04-28 17:06:04 -0600

    Requiring an experience be set for Experience VC

--- EscaPAID/View Controllers/ExperienceVC.swift
+++ EscaPAID/View Controllers/ExperienceVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ExperienceVC: UIViewController, UIPageViewControllerDataSource {
     
-    var experience:Experience?
+    var experience:Experience!
     
     var reviewManager: ReviewManager = ReviewManager()
 
@@ -19,10 +19,9 @@ class ExperienceVC: UIViewController, UIPageViewControllerDataSource {
     @IBOutlet weak var experienceTitle: UILabel!
     @IBOutlet weak var skillLevel: UILabel!
     
+    @IBOutlet weak var experienceDescription: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var includes: UILabel!
-    
-    @IBOutlet weak var testIncludes: UILabel!
     
     @IBOutlet weak var curatorImage: UIImageView!
     @IBOutlet weak var curatorName: ThemedLabel!
@@ -39,29 +38,29 @@ class ExperienceVC: UIViewController, UIPageViewControllerDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        if let experience = experience {
-            experienceTitle.text = experience.title
-            skillLevel.text = experience.skillLevel + " Level"
-            
-            
-            price.text = String(format: "$%.02f", experience.price)
-            includes.text = experience.includes
-            
-            testIncludes.text = experience.experienceDescription
-            
-            if let profileImageURL = URL(string: experience.curator.profileImageUrl) {
-                curatorImage.af_setImage(withURL: profileImageURL)
-            }
-            
-            curatorName.text = experience.curator.fullName
-            curatorAboutMe.text = experience.curator.aboutMe
-           
-            setFavoritesButtonState()
-            
-            reviewManager.fillReviews(experienceId: experience.id) { (_) in
-                // Heavy hammer. Reload the entire table for every new review.
-                self.tableView.reloadData()
-            }
+        guard experience != nil else {
+            // This page must have an experience set
+            return
+        }
+        experienceTitle.text = experience.title
+        skillLevel.text = experience.skillLevel + " Level"
+        
+        experienceDescription.text = experience.experienceDescription
+        price.text = String(format: "$%.02f", Double(experience.price) / 100.0)
+        includes.text = experience.includes
+        
+        if let profileImageURL = URL(string: experience.curator.profileImageUrl) {
+            curatorImage.af_setImage(withURL: profileImageURL)
+        }
+        
+        curatorName.text = experience.curator.fullName
+        curatorAboutMe.text = experience.curator.aboutMe
+       
+        setFavoritesButtonState()
+        
+        reviewManager.fillReviews(experienceId: experience.id) { (_) in
+            // Heavy hammer. Reload the entire table for every new review.
+            self.tableView.reloadData()
         }
         
         imagePageViewController?.dataSource = self
@@ -119,12 +118,12 @@ class ExperienceVC: UIViewController, UIPageViewControllerDataSource {
     
     @IBAction func messageButton_click(_ sender: Any) {
         
-        guard self.experience?.curator != FirebaseManager.user else {
+        guard self.experience.curator.uid != FirebaseManager.user!.uid else {
             present(UIAlertController(message: "You cannot send a message to yourself."), animated: true)
             return
         }
         
-        let data = ["user" : (self.experience?.curator)!]
+        let data = ["user" : self.experience.curator]
         
         // Send a broadcast notification to let the inbox know which thread to show
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: ThreadsNavigationController.SHOW_THREAD_POST), object: data)
@@ -144,26 +143,26 @@ class ExperienceVC: UIViewController, UIPageViewControllerDataSource {
         
         var index = (viewController as! ExperienceImageVC).pageIndex
         index += 1
-        if (index == experience?.imageUrls.count) {
+        if (index == experience.imageUrls.count) {
             return nil
         }
         return viewControllerAtIndex(index: index)
     }
     
     func viewControllerAtIndex(index: Int) -> ExperienceImageVC? {
-        if ((experience?.imageUrls.count == 0) || index >= (experience?.imageUrls.count)!) {
+        if ((experience.imageUrls.count == 0) || index >= (experience.imageUrls.count)) {
             return nil
         }
         
         let experienceImageVC: ExperienceImageVC = self.storyboard?.instantiateViewController(withIdentifier: "ExperienceImageVC") as! ExperienceImageVC
         experienceImageVC.pageIndex = index
-        experienceImageVC.imageURL = (experience?.imageUrls[index])!
+        experienceImageVC.imageURL = experience.imageUrls[index]
         
         return experienceImageVC;
     }
     
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        return (experience?.imageUrls.count)!
+        return experience.imageUrls.count
     }
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         // Start at index 0
